b2777e1a9526886c1f65ddebd24dc60388ba2ce3
aniruddhakide89-dev <aniruddhakide89@gmail.com>
2026-06-13 14:04:22 +0530

    Add user service

---
+++ user-service/src/main/java/com/ecommerce/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class,args);
+        }
+    }
